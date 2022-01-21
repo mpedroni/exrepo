30fa79cbf01c7aa08770ee2a01884b1d5f7fabdf
Matheus Pedroni <mpedroni77@gmail.com>
2022-01-21 13:48:04 -0300

    Include Tesla HTTP client

--- mix.exs
+++ mix.exs
@@ -42,7 +42,9 @@ defmodule Exrepo.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:tesla, "~> 1.4"},
+      {:hackney, "~> 1.17"}
     ]
   end
 
